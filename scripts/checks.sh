5440d9b9b5d0493d85ba7a0eff8dfcefb7467601
Andrew Buchan <andyhasit@gmail.com>
2024-08-13 23:04:50 +0100

    scripts

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
-# pre-commit run --all-files
-# pytest
+# Ensure main is up to date and all changes are in our branch
 git checkout main
 git pull
 git checkout @{-1}
+git rebase main || git rebase --abort
+
+
+# pre-commit run --all-files
+# pytest
